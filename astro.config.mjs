1c26966dff25e3c95c0f840a98dd9bf1d05414b9
Tom Durrant <tomchdurrant@gmail.com>
2024-01-22 00:10:45 +0000

    Add a bit extra to showcase functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,34 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://video-wiki.pages.dev",
+  integrations: [
+    starlight({
+      title: "Video Design",
+      social: {
+        discord: "https://discord.gg/PcpKkPpW2m",
+        // github: "https://github.com/withastro/starlight",
+      },
+      editLink: {
+        baseUrl: "https://github.com/tomtomdurrant/video-wiki/edit/main",
+      },
+      lastUpdated: true,
+
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
